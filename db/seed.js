ff8ffcd9aceb6204b19f8f44e102f15a08fe1da7
daltonhart <daltonvaren@gmail.com>
2019-10-09 21:15:53 -0700

    adds documentation for pokedex

--- db/seed.js
+++ db/seed.js
@@ -2,9 +2,11 @@ const db = require('../models');
 
 const pokemonList = require('./pokemon.json');
 
-// removes all pokemon and adds
+// removes all pokemon 
 db.Pokemon.remove({}, () => {
+	// loops through the json file
 	pokemonList.forEach(pokemon => {
+		// for each one creates a pokemon entry in the DB
 		db.Pokemon.create(pokemon, (error, createdPokemon) => {
 			if (error) return console.log(error);
 			console.log(createdPokemon);
