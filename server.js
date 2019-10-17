ca8cee7cbcd0a9f7df642ab269f72d30d71aec37
daltonhart <daltonvaren@gmail.com>
2019-10-17 10:33:30 -0700

    adjusted notes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 // internal modules
-const db = require("./models");
+const db = require('./models');
 // instanced module
 const app = express();
 
@@ -19,18 +19,18 @@ const PORT = process.env.PORT || 3000;
 // ------------------------------ROUTES
 
 //SECTION  View Routes
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
   // const file = `${__dirname}/views/index.html`
-  res.sendFile('views/index.html' , { root : __dirname});
+  res.sendFile('views/index.html', { root: __dirname });
 });
 
 // route, callback function(request,response)
 // app.get('route', function(request, response))
-app.get('/api/v1', (req,res) =>{
+app.get('/api/v1', (req, res) => {
   res.json({
-    status:200,
+    status: 200,
     message: 'Welcome to the Pokedex Api.',
-    endpoints:[
+    endpoints: [
       {
         method: 'GET',
         path: '/api/v1',
@@ -43,9 +43,9 @@ app.get('/api/v1', (req,res) =>{
 //SECTION  Pokemon Routes
 
 // Index route
-app.get('/api/v1/pokemon', (req,res)=>{
-  db.Pokemon.find({},(error,allPokemon)=>{
-    if(error) return console.log(error);
+app.get('/api/v1/pokemon', (req, res) => {
+  db.Pokemon.find({}, (error, allPokemon) => {
+    if (error) return console.log(error);
     res.json({
       status: 200,
       count: allPokemon.length,
@@ -56,9 +56,9 @@ app.get('/api/v1/pokemon', (req,res)=>{
 });
 
 // Show route
-app.get('/api/v1/pokemon/:name', (req,res)=>{
-  db.Pokemon.findOne({name:req.params.name}, (error, foundPokemon)=>{
-    if(error) return console.log(error);
+app.get('/api/v1/pokemon/:name', (req, res) => {
+  db.Pokemon.findOne({ name: req.params.name }, (error, foundPokemon) => {
+    if (error) return console.log(error);
     res.json({
       status: 200,
       data: foundPokemon,
@@ -67,11 +67,9 @@ app.get('/api/v1/pokemon/:name', (req,res)=>{
   });
 });
 
-//TODO get all trainers that have a specific Pokemon
-
 // Create route
-app.post('/api/v1/pokemon', (req,res)=>{
-  db.Pokemon.create(req.body, (error, createdPokemon)=>{
+app.post('/api/v1/pokemon', (req, res) => {
+  db.Pokemon.create(req.body, (error, createdPokemon) => {
     if (error) return console.log(error);
     res.json({
       status: 201,
@@ -84,25 +82,23 @@ app.post('/api/v1/pokemon', (req,res)=>{
 //SECTION  Trainer Routes
 
 // Index Route
-app.get('/api/v1/trainers', (req,res)=>{
+app.get('/api/v1/trainers', (req, res) => {
   db.Trainer.find({})
     .populate('pokemon')
-    .exec((error, allTrainers)=>{
-      if(error) return console.log(error);
+    .exec((error, allTrainers) => {
+      if (error) return console.log(error);
       res.json({
-        status:200,
+        status: 200,
         count: allTrainers.length,
         data: allTrainers,
         requestedAt: new Date().toLocaleString()
       });
-    })
+    });
 });
 
-//TODO Dynamic Update Route
-
 // Create Route
-app.post('/api/v1/trainers', (req,res)=>{
-  db.Trainer.create(req.body, (error, createdTrainer)=>{
+app.post('/api/v1/trainers', (req, res) => {
+  db.Trainer.create(req.body, (error, createdTrainer) => {
     if (error) return console.log(error);
     res.json({
       status: 201,
@@ -111,9 +107,6 @@ app.post('/api/v1/trainers', (req,res)=>{
     });
   });
 });
-
-//TODO get all trainers that have a specific Badge
-
 
 // ------------------------------Start Server
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}/`));
